f5ac83fdbef7daf72b861ed7e8d3ce37af84923b
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-10-12 14:16:40 +0200

    send_file: handle sending multiple files

--- send_file.py
+++ send_file.py
@@ -8,8 +8,8 @@ import sys
 config = boox.read_config()
 send2boox = boox.Boox(token=config['default']['token'])
 
-if len(sys.argv) == 2:
-    file_to_send = sys.argv[1]
-    send2boox.send_file(file_to_send)
+if len(sys.argv) >= 2:
+    for file_to_send in sys.argv[1:]:
+        send2boox.send_file(file_to_send)
 
 send2boox.list_files()
